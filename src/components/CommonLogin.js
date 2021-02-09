2b3e5b30ebe13bbb46eccd85f146a5b3d831f378
seolleung2 <seolleung22@gmail.com>
2021-02-10 05:18:02 +0900

    [feat] Login Success - Login.js, CommonLogin.js 에 axios 비동기 요청(로그인요청) 진행 완료 - 서버에서 내려주는 token 을 state 에 저장 - 로그인 시 화면이 바뀌는 모습을 보여주기 위해 임시로 Lobby.js 파일   생성 - eslint 에서 no-unused-vars, react/prop-types 해제함

--- src/components/CommonLogin.js
+++ src/components/CommonLogin.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function CommonLogin() {
+const axios = require('axios');
+
+export default function CommonLogin(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignin = () => {
+    // console.log(props.loginHandler);
+    axios
+      .post(
+        'http://localhost:4000/users/login',
+        {
+          email,
+          password,
+        },
+        {
+          'Content-Type': 'application/json',
+        },
+      )
+      .then((res) => props.loginHandler(res.data.token))
+      .catch((err) => console.log(err.response));
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
   return (
     <div className="wrap-common">
-      <form action="#" className="table-login" method="post">
+      <form
+        action="#"
+        className="table-login"
+        method="post"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <label htmlFor="inputEmail">Email</label>
-        <input id="inputEmail" type="email" />
+        <input
+          id="inputEmail"
+          type="email"
+          defaultValue={email}
+          onChange={onChangeEmail}
+        />
         <label htmlFor="inputPassword">Password</label>
-        <input id="inputPassword" type="password" />
-        <button className="btn-login" type="button">
+        <input
+          id="inputPassword"
+          type="password"
+          defaultValue={password}
+          onChange={onChangePassword}
+        />
+        <button className="btn-login" type="button" onClick={handleSignin}>
           Login
         </button>
       </form>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CommonLogin from '../components/CommonLogin';
 import SocialLogin from '../components/SocialLogin';
 import GuestLogin from '../components/GuestLogin';
 import TopLogo from '../components/TopLogo';
 import Logo from '../assets/img/logo.svg';
 
+import Lobby from './Lobby';
+
 export default function Login() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [accessToken, setAccessToken] = useState('');
+
+  const loginHandler = (data) => {
+    setIsLogin(true);
+    setAccessToken(data);
+  };
+
   return (
     <>
-      <TopLogo />
-      <div className="wrap-login">
-        <h1 className="logo mobile">
-          <img className="img-logo" src={Logo} alt="Hotair" />
-        </h1>
-        <h2 className="welcome semi-title">Welcome!</h2>
-        <section className="login big-round">
-          <CommonLogin />
-          <SocialLogin />
-        </section>
-        <GuestLogin />
-      </div>
+      {isLogin ? (
+        <Lobby accessToken={accessToken} />
+      ) : (
+        <>
+          <TopLogo />
+          <div className="wrap-login">
+            <h1 className="logo mobile">
+              <img className="img-logo" src={Logo} alt="Hotair" />
+            </h1>
+            <h2 className="welcome semi-title">Welcome!</h2>
+            <section className="login big-round">
+              <CommonLogin loginHandler={loginHandler} />
+              <SocialLogin />
+            </section>
+            <GuestLogin />
+          </div>
+        </>
+      )}
     </>
   );
 }
