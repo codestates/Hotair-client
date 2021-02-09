1e0e0761d49ebf479155fa7c186a7ac5dc274343
seolleung2 <seolleung22@gmail.com>
2021-02-09 22:00:28 +0900

    [feat] Add UserInfo Component. - 리액트 코드 변경사항 적용 - userinfo 렌더 코드 어떻게 세분화 할지 회의 체크 후 진행예정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import UserInfo from './pages/UserInfo';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,6 +18,7 @@ function App() {
         <Switch>
           <Route path="/users/login" component={Login} exact />
           <Route path="/users/signup" component={Signup} exact />
+          <Route path="/users/userinfo" component={UserInfo} exact />
           <Route path="/" render={() => <Redirect to="/users/login" />} />
         </Switch>
       </Router>
